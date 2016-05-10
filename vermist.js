c6b81b50483b18df0e582e81683720b149a07cb6
Franklin van de Meent <fr@nkl.in>
2016-05-10 03:10:08 +0200

    Complete rewrite
    No post-processing implemented yet

--- vermist.js
+++ vermist.js
@@ -1,256 +1,209 @@
 /*
-Name:		vermist.js - Access missing persons on Politie.nl
-Source:		https://github.com/fvdm/nodejs-vermist
-Feedsback:	https://github.com/fvdm/nodejs-vermist/issues
-License:	Unlicense / Public Domain
-
-This is free and unencumbered software released into the public domain.
-
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
-
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to <http://unlicense.org>
+Name:         vermist
+Description:  Node.js module to retrieve missing persons from Politie.nl
+Author:       Franklin van de Meent (https://frankl.in)
+Source:       https://github.com/fvdm/nodejs-vermist
+Feedsback:    https://github.com/fvdm/nodejs-vermist/issues
+License:      Unlicense (public domain, see UNLICENSE file)
 */
 
-var http = require('http'),
-    querystring = require('querystring')
+var httpreq = require ('httpreq');
+
+var config = {
+  timeout: 5000
+};
 
 
-module.exports = function() {
-	
-	// Persoon informatie
-	// vermist ( persoonURL, callback )
-	
-	if( arguments[0].match( /^http:\/\// ) ) {
-		var callback = arguments[1]
-		var path = arguments[0].replace( 'http://www.politie.nl/', '' )
-		
-		talk( path, function( err, data ) {
-			if( !err ) {
-				
-				var persoon = {}
-				
-				data.replace( /<div class="contentDetail wanted">[\s\n]*<h1>Vermist<\/h1><h2>([^<]+)<\/h2>/, function( s, naam ) {
-					persoon.naam = naam
-				})
-				
-				data.replace( /<th>Laatste update:<\/th>[\s\n]*<td>([0-9]{2}\-[0-9]{2}\-[0-9]{4}) \| ([0-9]{2}:[0-9]{2})<\/td>/, function( s, datum, tijd ) {
-					persoon.updateDatum = datum
-					persoon.updateTijd = tijd
-				})
-				
-				data.replace( /<th>Zaaknummer:<\/th>[\s\n]*<td>([^<]+)<\/td>/, function( s, zaak ) {
-					persoon.zaaknummer = zaak
-				})
-				
-				data.replace( /<th>Vermist sinds:<\/th><td>([0-9]{2}\-[0-9]{2}\-[0-9]{4})<\/td>/, function( s, datum ) {
-					persoon.vermistDatum = datum
-				})
-				
-				data.replace( /<th>Laatst gezien in:<\/th><td>([^<]+)<\/td>/, function( s, locatie ) {
-					persoon.vermistLocatie = locatie
-				})
-				
-				data.replace( /<a rel="group-media-gallery" class="gallery-fancybox" href="([^"]+)" title="[^"]+">/, function( s, foto ) {
-					persoon.fotoGroot = 'http://www.politie.nl'+ foto
-				})
-				
-				data.replace( /<img class="profile" src="([^"]+)" alt="[^"]*"\/>/, function( s, foto ) {
-					persoon.fotoKlein = 'http://www.politie.nl'+ foto
-				})
-				
-				data.replace( /<p>([^<]+)<\/p>/g, function( s, tekst ) {
-					tekst = tekst.replace( /[\r\n]+/, ' ' )
-					if( persoon.omschrijving === undefined ) {
-						persoon.omschrijving = [tekst]
-					} else {
-						persoon.omschrijving.push( tekst )
-					}
-				})
-				
-				data.replace( /<a href="([^"]+)" class="continue">Naar het tipformulier<\/a>/, function( s, tip ) {
-					persoon.tipformulier = tip
-				})
-				
-				data.replace( /<th>Voornaam:<\/th>[\s\n]*<td>([^<]+)<\/td>/, function( s, voornaam ) {
-					persoon.voornaam = voornaam
-				})
-				
-				data.replace( /<th>Naam:<\/th>[\s\n]*<td>([^<]+)<\/td>/, function( s, naam ) {
-					persoon.achternaam = naam
-				})
-				
-				data.replace( /<th>Geboortedatum:<\/th>[\s\n]*<td>([^<]+)<\/td>/, function( s, geb ) {
-					persoon.geboorteDatum = geb
-				})
-				
-				data.replace( /<th>Huidige leeftijd:<\/th>[\s\n]*<td>([^<]+)<\/td>/, function( s, leeftijd ) {
-					persoon.huidigeLeeftijd = leeftijd
-				})
-				
-				data.replace( /<th>Lengte:<\/th>[\s\n]*<td>([^<]+)<\/td>/, function( s, lengte ) {
-					persoon.lengte = lengte
-				})
-				
-				data.replace( /<th>Haarkleur:<\/th>[\s\n]*<td>([^<]+)<\/td>/, function( s, haarkleur ) {
-					persoon.haarkleur = haarkleur
-				})
-				
-				data.replace( /<th>Oogkleur:<\/th>[\s\n]*<td>([^<]+)<\/td>/, function( s, oogkleur ) {
-					persoon.oogkleur = oogkleur
-				})
-				
-				data.replace( /<th>Geslacht:<\/th>[\s\n]*<td>([^<]+)<\/td>/, function( s, geslacht ) {
-					persoon.geslacht = geslacht
-				})
-				
-				callback( null, persoon )
-				
-			} else {
-				callback( err )
-			}
-		})
-		
-	} else {
-	
-		// Personen lijst of zoeken
-		// vermist ( categorie, [props], callback )
-		
-		// Eén $page bevat max 16 personen
-		
-		// CATEGORIE:
-		//	alles
-		//	kinderen
-		//	volwassenen
-		//	ongeidentificeerd
-		
-		// PROPS:
-		//	page		: pagina nummer
-		// 	query		: trefwoorden
-		// 	geoquery	: locatie
-		// 	distance	: afstand van `geoquery`  in KM, bijv. 2.0
-		
-		if( arguments.length == 2 ) {
-			// lijst
-			var categorie = arguments[0]
-			var callback = arguments[1]
-		} else if( arguments.length == 3 ) {
-			// zoeken
-			var categorie = arguments[0]
-			var props = arguments[1]
-			var callback = arguments[2]
-		}
-		
-		switch( categorie ) {
-			case 'kinderen':		var cat = '/vermiste-kinderen'; break
-			case 'volwassenen':		var cat = '/vermiste-volwassenen'; break
-			case 'ongeidentificeerd':	var cat = '/ongeidentificeerd'; break
-			case 'alles': default:		var cat = ''; break
-		}
-		
-		talk( 'vermist'+ cat, props, function( err, data ){
-			if( !err ) {
-				var personen = []
-				
-				data.replace( /<li class="profileBlock[^"]*">[\s\n]+<a href="(\/vermist\/([^\/]+)\/([0-9]{4})\/([a-z]+)\/[^\.]+\.html)" title="[^"]+">[\s\n]+<img class="profile" src="([^"]+)" alt="[^"]+"\/>[\s\n]+<span class="definition">([^<]+)<\/span>[\s\n]+Sinds: ([0-9]{2}\-[0-9]{2}\-[0-9]{4})<br\/>Laatst gezien in: ([^<]+)<\/a>[\s\n]+<\/li>/g, function( s, path, cat, jaar, maand, image, naam, sinds, locatie ) {
-					personen.push({
-						naam:		naam,
-						url:		'http://www.politie.nl'+ path,
-						thumbnail:	'http://www.politie.nl'+ image,
-						categorie:	cat,
-						vermistJaar:	jaar,
-						vermistMaand:	maand,
-						vermistDatum:	sinds,
-						vermistLocatie:	locatie
-					})
-				})
-				
-				callback( null, personen )
-			} else {
-				callback( err )
-			}
-		})
-	}
+/**
+ * Process httpreq response
+ *
+ * @callback callback
+ * @param err {Error, null} - httpreq error
+ * @param res {object} - httpreq response details
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function processResponse (err, res, callback) {
+  var data = res && res.body || null;
+  var error = null;
+
+  if (res.statusCode >= 300) {
+    error = new Error('remote error');
+    error.statusCode = res.statusCode;
+    error.headers = res.headers;
+    error.body = data;
+  }
+
+  if (res.statusCode === 404) {
+    error.message = 'not found';
+  }
+
+  if (error) {
+    callback (error);
+    return;
+  }
+
+  callback (null, data);
 }
 
-// Communicatie
-function talk( path, props, callback ) {
-	if( typeof props === 'function' ) {
-		var callback = props
-		var props = {}
-	}
-	
-	if( typeof props !== 'object' ) {
-		props = {}
-	}
-	
-	path = '/'+ path +'?'+ querystring.stringify( props )
-	
-	var options = {
-		host:		'www.politie.nl',
-		port:		80,
-		path:		path,
-		method:		'GET',
-		headers: {
-			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.52 Safari/537.17'
-		}
-	}
-	
-	var request = http.request( options )
-	
-	request.on( 'response', function( response ) {
-		var data = ''
-		response.on( 'data', function( ch ) { data += ch })
-		response.on( 'close', function() { callback( new Error('connection dropped') ) })
-		response.on( 'end', function() {
-			data = data.toString('utf8').trim()
-			
-			if( response.statusCode == 404 ) {
-				var err = new Error('not found')
-				err.request = options
-				err.response = {
-					headers: response.headers,
-					body: data
-				}
-				callback( err )
-			} else if( response.statusCode >= 300 ) {
-				var err = new Error('service error')
-				err.request = options
-				err.response = {
-					headers: response.headers,
-					body: data
-				}
-				callback( err )
-			} else {
-				callback( null, data )
-			}
-		})
-	})
-	
-	request.on( 'error', function( error ) {
-		var err = new Error('request failed')
-		err.request = options
-		err.details = error
-		callback( err )
-	})
-	
-	request.end()
+
+/**
+ * Communication
+ *
+ * @callback callback
+ * @param path {string} - Request path after 'www.politie.nl/gezocht-en-vermist'
+ * @param [params] {object} - Request parameters
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function talk (path, params, callback) {
+  var options = {
+    url: 'https://www.politie.nl' + path,
+    parameters: params,
+    method: 'GET',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.52 Safari/537.17'
+    }
+  };
+
+  httpreq.doRequest (options, function (err, res) {
+    processResponse (err, res, callback);
+  });
 }
+
+
+/**
+ * Process a person
+ *
+ * @callback callback
+ * @param err {Error, null} - Error
+ * @param [data] {object} - Response data
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function processPerson (err, data, callback) {
+  if (err) {
+    callback (err);
+    return;
+  }
+
+  callback (null, data);
+}
+
+
+/**
+ * Process a category
+ *
+ * @callback callback
+ * @param err {Error, null} - Error
+ * @param [data] {object} - Response data
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function processCategory (err, data, callback) {
+  if (err) {
+    callback (err);
+    return;
+  }
+
+  callback (null, data);
+}
+
+
+/**
+ * Process search result
+ *
+ * @callback callback
+ * @param err {Error, null} - Error
+ * @param [data] {object} - Response data
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function processSearch (err, data, callback) {
+  if (err) {
+    callback (err);
+    return;
+  }
+
+  callback (null, data);
+}
+
+
+/**
+ * Method
+ *
+ * @callback callback
+ * @param input {string} - URL to person or Category name
+ * @param [params] {object} - Query parameters
+ * @param callback {function} - `function (err, data) {}`
+ * @returns {void}
+ */
+
+function methodGet (input, params, callback) {
+  var path = '';
+  var cats = [
+    'gezochte-personen',
+    'nationale-opsporingslijst',
+    'eigenaar-gezocht',
+    'vermiste-kinderen',
+    'vermiste-volwassenen',
+    'niet-geidentificeerde-personen'
+  ];
+
+  if (typeof params === 'function') {
+    callback = params;
+    params = {};
+  }
+
+  input = String (input);
+
+  // Person URL
+  if (input.match (/^https:\/\//)) {
+    path = input.replace ('https://www.politie.nl', '');
+
+    talk (path, function (err, data) {
+      processPerson (err, data, callback);
+    });
+
+    return;
+  }
+
+  // Search
+  if (input === 'search' && Object.keys (params).length) {
+    path = '/zoek';
+
+    talk (path, params, function (err, data) {
+      processSearch (err, data, callback);
+    });
+
+    return;
+  }
+
+  // Category name
+  if (cats.indexOf (input) >= 0) {
+    path = '/gezocht-en-vermist/' + input;
+  }
+
+  talk (path, function (err, data) {
+    processCategory (err, data, callback);
+  });
+}
+
+
+/**
+ * Module interface
+ *
+ * @param [conf] {object}
+ * @param [conf.timeout} {number} - Request time out in ms, 1000 = 1 sec
+ * @returns methodGet {function}
+ */
+
+module.exports = function (conf) {
+  config.timeout = conf.timeout || config.timeout;
+  return methodGet;
+};
