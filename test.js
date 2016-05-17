fad9c6e19dcd991aea8a09ce7c3b0de819aae50a
Franklin van de Meent <fr@nkl.in>
2016-05-17 19:56:34 +0200

    Test: added more content checks

--- test.js
+++ test.js
@@ -28,15 +28,38 @@ dotest.add ('Module', function (test) {
 dotest.add ('Method processPerson', function (test) {
   vermist (person, function (err, data) {
     var article = data && data.article;
+    var location = data && data.location;
+    var center = location && location.center;
+    var markers = location && location.markers;
+    var marker = markers && markers[0];
 
     test (err)
       .isObject ('fail', 'data', data)
       .isObject ('fail', 'data.article', article)
       .isObject ('fail', 'data.article.image', article && article.image)
       .isObject ('fail', 'data.article.link', article && article.link)
+      .isString ('fail', 'data.article.title', article && article.title)
+      .isNotEmpty ('warn', 'data.article.title', article && article.title)
+      .isString ('fail', 'data.article.language', article && article.language)
+      .isNotEmpty ('warn', 'data.article.language', article && article.language)
+      .isString ('fail', 'data.article.caseNumber', article && article.caseNumber)
+      .isRegexpMatch ('warn', 'data.article.caseNumber', article && article.caseNumber, /^\d+$/)
       .isString ('fail', 'data.article.pretext', article && article.pretext)
-      .isObject ('fail', 'data.location', data && data.location)
-      .info (data);
+      .isObject ('fail', 'data.location', location)
+      .isObject ('fail', 'data.location.center', center)
+      .isString ('fail', 'data.location.center.latitude', center && center.latitude)
+      .isNotEmpty ('warn', 'data.location.center.latitude', center && center.latitude)
+      .isString ('fail', 'data.location.center.longitude', center && center.longitude)
+      .isNotEmpty ('warn', 'data.location.center.longitude', center && center.longitude)
+      .isArray ('fail', 'data.location.markers', markers)
+      .isObject ('fail', 'data.location.markers[0]', marker)
+      .isString ('fail', 'data.location.markers[0].latitude', marker && marker.latitude)
+      .isNotEmpty ('warn', 'data.location.markers[0].latitude', marker && marker.latitude)
+      .isString ('fail', 'data.location.markers[0].longitude', marker && marker.longitude)
+      .isNotEmpty ('warn', 'data.location.markers[0].longitude', marker && marker.longitude)
+      .isString ('fail', 'data.location.image', location && location.image)
+      .isNotEmpty ('warn', 'data.location.image', location && location.image)
+      .done ();
   });
 });
 
